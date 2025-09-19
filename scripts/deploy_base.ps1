63eeb3c1caef6c9ce0fbbb41c12605b8acc139f4
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-19 22:14:36 +0100

    Add code for the project

--- scripts/deploy_base.ps1
+++ scripts/deploy_base.ps1
@@ -19,18 +19,16 @@ docker pull ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
 $existingContainer = docker ps -a --filter "name=$containerName" --format "{{.Names}}"
 
 if ($existingContainer) {
-    # Vérifier s’il est en cours d’exécution
+    # Si le container existe mais n'est pas en cours d'exécution, le démarrer
     $runningContainer = docker ps --filter "name=$containerName" --format "{{.Names}}"
-    if ($runningContainer) {
-        docker stop $containerName
+    if (-not $runningContainer) {
+        docker start $containerName
+        Write-Host "Container $containerName started"
     } else {
-        Write-Host "Container $containerName exists but is not running, skipping stop"
+        Write-Host "Container $containerName is already running"
     }
-    docker rm $containerName
 } else {
-    Write-Host "No container named $containerName exists, skipping remove"
+    # Si le container n'existe pas, le créer et le lancer
+    docker run -d --name $containerName --network $networkName ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
+    Write-Host "Container $containerName created and started"
 }
-
-
-# Run le conteneur
-docker run -d --name $containerName --network $networkName ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
