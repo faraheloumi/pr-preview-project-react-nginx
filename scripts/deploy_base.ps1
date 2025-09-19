df2dcdfc1500582cb8732c466fd01739c62a76c8
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-19 20:58:27 +0100

    Add code for the project

--- scripts/deploy_base.ps1
+++ scripts/deploy_base.ps1
@@ -1,9 +1,9 @@
 $networkName = "pr-preview-net"
 $containerName = "base-web"
 
-# Vérifier si le réseau existe
+# Créer le réseau seulement s'il n'existe pas
 $existingNetwork = docker network ls --filter "name=$networkName" --format "{{.Name}}"
-if ($existingNetwork -ne $networkName) {
+if (-not $existingNetwork) {
     docker network create $networkName --driver bridge
 } else {
     Write-Host "Network $networkName already exists"
@@ -17,23 +17,19 @@ docker pull ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
 
 # Vérifier si le container existe
 $existingContainer = docker ps -a --filter "name=$containerName" --format "{{.Names}}"
-if ($existingContainer -eq $containerName) {
+
+if ($existingContainer) {
     # Vérifier s’il est en cours d’exécution
-    $running = docker ps --filter "name=$containerName" --format "{{.Names}}"
-    if ($running -eq $containerName) {
+    $runningContainer = docker ps --filter "name=$containerName" --format "{{.Names}}"
+    if ($runningContainer) {
         docker stop $containerName
     } else {
-        Write-Host "Container $containerName exists but is not running"
+        Write-Host "Container $containerName exists but is not running, skipping stop"
     }
     docker rm $containerName
 } else {
-    Write-Host "No container named $containerName exists"
+    Write-Host "No container named $containerName exists, skipping remove"
 }
 
 # Run le conteneur
 docker run -d --name $containerName --network $networkName ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
-
-# Reload NGINX
-docker exec nginx nginx -s reload
-
-
