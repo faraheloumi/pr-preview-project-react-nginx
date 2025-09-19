4da5c46843b0651cc83063cfc6cebc0ec181cb5e
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-19 21:27:25 +0100

    Add code for the project

--- scripts/deploy_base.ps1
+++ scripts/deploy_base.ps1
@@ -1,36 +1,14 @@
-$networkName = "pr-preview-net"
-$containerName = "base-web"
+# deploy_base.ps1
 
-# Créer le réseau seulement s'il n'existe pas
-$existingNetwork = docker network ls --filter "name=$networkName" --format "{{.Name}}"
-if (-not $existingNetwork) {
-    docker network create $networkName --driver bridge
-} else {
-    Write-Host "Network $networkName already exists"
-}
+# Chemin vers le docker-compose.yml
+$composeFile = "..\infra\docker-compose.yml"
+
 
 # Login GHCR
 docker login ghcr.io -u $env:GITHUB_ACTOR -p $env:GHCR_PAT
 
-# Pull image
-docker pull ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
+# Pull les images définies dans le docker-compose
+docker-compose -f $composeFile pull
 
-# Vérifier si le container existe
-$existingContainer = docker ps -a --filter "name=$containerName" --format "{{.Names}}"
-
-if ($existingContainer) {
-    # Vérifier s’il est en cours d’exécution
-    $runningContainer = docker ps --filter "name=$containerName" --format "{{.Names}}"
-    if ($runningContainer) {
-        docker stop $containerName
-    } else {
-        Write-Host "Container $containerName exists but is not running, skipping stop"
-    }
-    docker rm $containerName
-} else {
-    Write-Host "No container named $containerName exists, skipping remove"
-}
-
-
-# Run le conteneur
-docker run -d --name $containerName --network $networkName ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:latest
+# Lancer tous les services en arrière-plan
+docker-compose -f $composeFile up -d
