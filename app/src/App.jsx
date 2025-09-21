aba836723caa1cf118ecad75a6714b08d5f45ce7
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-21 07:33:38 +0100

    Test workflows

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 export default function App(){ 
   return <div style={{fontFamily:'sans-serif',padding:20}}>
     <h1>Preview demo</h1>
-    <p>This is a PR preview build11.</p>
+    <p>This is a PR preview build13.</p>
   </div>;
 }
