e50721ac291b5b94a4d676b0338943643f7aad70
faraheloumi <farah.elloumi2000@gmail.com>
2025-09-21 05:59:13 +0100

    Deploy preview

--- scripts/remove_preview.ps1
+++ scripts/remove_preview.ps1
@@ -1,7 +1,7 @@
 param([string]$PR_NUM)
 
-docker stop pr-$PR_NUM -ErrorAction SilentlyContinue
-docker rm pr-$PR_NUM -ErrorAction SilentlyContinue
+docker stop web-pr-$PR_NUM -ErrorAction SilentlyContinue
+docker rm web-pr-$PR_NUM -ErrorAction SilentlyContinue
 Remove-Item nginx\sites-enabled\pr-$PR_NUM.conf -ErrorAction SilentlyContinue
 docker exec nginx nginx -s reload
 docker rmi ghcr.io/faraheloumi/pr-preview-project-react-nginx/web:pr-$PR_NUM -ErrorAction SilentlyContinue
